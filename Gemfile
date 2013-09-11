f7cbe604e1447365d1a1c0cd68b385a6b2cee328
Vyacheslav <sadnerp@gmail.com>
2013-09-11 21:59:09 +0400

    initial

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'slim'
+
+gem 'bcrypt-ruby', '3.0.1'
+gem 'will_paginate', '3.0.3'
+gem 'will_paginate-bootstrap'
+
+group :development, :test do
+	gem 'sqlite3'
+end
+
+group :development do
+	gem 'annotate'
+end
+
+group :assets do
+	gem 'sass-rails'
+	gem 'coffee-rails'
+	gem 'uglifier'
+	gem 'anjlab-bootstrap-rails',	:require	=> 'bootstrap-rails',
+									:github		=> 'anjlab/bootstrap-rails'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Space::Application.config.secret_token = 'e60407bf22108e4be87d6fcc40109b59bb0aa058c20ceccee2e2aedbec3f2d55afc8a0e58ff2d7c7d20cdfbba584863a69f224732585b4eb2926fc7203cff113'
